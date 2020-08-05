c610caf42c272050302fd17b43c2b43c2905f602
giljinoh <zhsks515ny@naver.com>
2020-08-05 22:46:08 +0900

    Update rule_driver_PIDcontrol.py

--- src/xycar_sim_drive/src/rule_driver_PIDcontrol.py
+++ src/xycar_sim_drive/src/rule_driver_PIDcontrol.py
@@ -8,99 +8,64 @@ from std_msgs.msg import Int32MultiArray
 
 xycar_sub = Int32MultiArray()
 xycar_sub.data = [0,0,0,0,0]
-timeList = [0]
-errorList = [0]
 accident = False
 
-
 def callback(msg):
-
     xycar_sub.data[0] = msg.data[0]
     xycar_sub.data[1] = msg.data[1]
     xycar_sub.data[2] = msg.data[2]
     xycar_sub.data[3] = msg.data[6]
     xycar_sub.data[4] = msg.data[7]
 
-    print(msg.data)
+    #print(msg.data)
   
 
 def collision():
     global accident
     for i in xycar_sub.data:
-
-       
-        if i < 48 + 2 and 0 < i: 
+        if i < 48 + 2 and 0 < i: ## radius of boundary circle = 20*sqrt(13) ## 2 = additional space
             accident = True
-
     return accident        
 
+def getTargetSensor(NEAR_WALL, right_sensor, left_sensor) :
 
-def get_errorControl(vector):
+    if NEAR_WALL == 'LEFT_WALL':
+        targetSensor = left_sensor
+    elif NEAR_WALL == 'RIGHT_WALL':
+        targetSensor = right_sensor
+    else:
+        targetSensor = left_sensor
+
+    return targetSensor
+
+def wallFollowing(targetSensor, VtoA):
+    global angle_cur
+
+    if targetSensor > 100:
+        angle_cur -= VtoA
+    elif targetSensor < 100:
+        angle_cur += VtoA
+    else:
+        angle_cur = 0
+
+def turnDirection(vector):
+    global angle_cur, value
+
+    VtoA = vector * 50 / 1000   # 50 (MAX angle) / MAX(vector)  
+
+    if vector > 0:
+        angle_cur = 0#VtoA
+        turn = 'RIGHT'
+
+    elif vector < 0:
+        angle_cur = 0#VtoA
+        turn = 'LEFT'
+
+    else:
+        turn = 'STRAIGHT'
+        angle_cur = 0    
     
-    global errorList
-    global error
-    
-    desired_vector = 0
-    # errorSum = 0
-    
-    error = vector - desired_vector
-
-    if error > 300:
-        error -= error * 2 / 3
-    print("error")
-    print(error)
-
-    errorList.insert(0, error)
-    errorPrev = errorList[1]
-
-    errorControl = error - errorPrev
-    
-    # if errorControl is not 0:
-    #     errorControl = 0.0
-
-    # print("errorControl")
-    # print(errorControl)
-    # if errorControl > 100:
-    #     errorControl = 100
-
-    errorList.pop()
-    
-    return errorControl
-
-
-def get_dt():
-    global timeList
-
-    error_Time = rospy.Time.from_sec(time.time())
-    timeList.insert(0, error_Time.to_sec())
-    errorPrev_Time = timeList[1]
-    dt = error_Time.to_sec() - errorPrev_Time
-    timeList.pop()
-
-    return dt
-
-
-
-def calculate_PID(error, errorControl, dt):
-
-    kp = 0.87                  # float(17) / 200
-    #ki = float(11) / 200
-    kd = float(17) / 200
-
-    integral_output = 0
-
-    proportional_output = kp * error 
-    #integral_output = integral_output + ki * error * dt
-    derivative_output = kd * (errorControl / dt)
-
-    presaturated_output = proportional_output + derivative_output 
-    # presaturated_output = proportional_output + derivative_output + integral_output
-
-    # print("presaturated_output")
-    # print(presaturated_output)
-
-    return presaturated_output
-
+    return turn
 
 # rth = ROSTopicHz.get_hz(topic=/ultrasonic) 
 rospy.init_node('guide')
@@ -111,60 +76,48 @@ xycar_msg = Int32MultiArray()
 
 while not rospy.is_shutdown():
 
-    val = 1.0
+    VtoA = 1.0
     angle_cur = 0
-    velocity = 100  
-    error = 0.0
+    velocity = 200
 
     right_sensor = xycar_sub.data[3] + xycar_sub.data[2]
     left_sensor = xycar_sub.data[4] + xycar_sub.data[0]
+
+    NEAR_WALL = 'TWO_WALLS'
 
     if left_sensor == 0 or right_sensor == 0:
         left_sensor = 1
         right_sensor = 1
    
     vector  = float(right_sensor) - float(left_sensor)
+    VtoA = vector * 50 / 1000
 
-
-    errorControl = get_errorControl(vector)
-    dt = get_dt()
-    presaturated_output = calculate_PID(error, errorControl, dt)
-
-
-    if vector > 0:
-        angle_cur = presaturated_output
-        turn = 'RIGHT'
-
-    elif vector < 0:
-        angle_cur = presaturated_output
-        turn = 'LEFT'
-
+    if right_sensor > left_sensor:
+        NEAR_WALL = 'LEFT_WALL'
+    elif right_sensor < left_sensor:
+        NEAR_WALL = 'RIGHT_WALL'
     else:
-        turn = 'STRAIGHT'
-        angle_cur = 0    
+        NEAR_WALL = 'TWO_WALLS'
 
+    targetSensor = getTargetSensor(NEAR_WALL, right_sensor, left_sensor)
+    wallFollowing(targetSensor, VtoA)
+    turn = turnDirection(vector)    
 
     if collision() == True:
         temp = min(xycar_sub.data)
         idx = xycar_sub.data.index(temp)
-
         while float(temp)+0.32 > xycar_sub.data[idx]: # 1 rate would be best!
-            velocity = -100
+            velocity = -200
             if turn == 'RIGHT':
-                angle_cur = -presaturated_output 
+                angle_cur = -VtoA  
             elif turn == 'LEFT':
-                angle_cur = -presaturated_output
+                angle_cur = -VtoA 
             else:
                 turn = 'STRAIGHT'
-                angle_cur = 0  
-            # print("collision presaturated_output")
-            # print(presaturated_output)  
+                angle_cur = 0    
             xycar_msg.data = [angle_cur, velocity]
             motor_pub.publish(xycar_msg)  
         accident = False    
-        # print("collision angle_cur")
-        # print(angle_cur)
-    # print("angle_cur")
-    # print(angle_cur)
+
     xycar_msg.data = [angle_cur, velocity]
     motor_pub.publish(xycar_msg)
