c89497974b512eea5319220edb19c988ad73a962
juhyung98 <choijh9182@naver.com>
2020-08-06 16:56:26 +0900

    final final

--- src/xycar_sim_drive/src/rule_driver.py
+++ src/xycar_sim_drive/src/rule_driver.py
@@ -2,96 +2,162 @@
 
 import rospy
 import math
-import time
 from std_msgs.msg import Int32MultiArray
-
-
-xycar_sub = Int32MultiArray()
-xycar_sub.data = [0,0,0,0,0]
-timeList = [0]
-errorList = [0]
-accident = False
 
 
 def callback(msg):
 
-    ## As we don't use index [3], [4], [5]
-    ## Make new data array
-
-    xycar_sub.data[0] = msg.data[0]
-    xycar_sub.data[1] = msg.data[1]
-    xycar_sub.data[2] = msg.data[2]
-    xycar_sub.data[3] = msg.data[6]
-    xycar_sub.data[4] = msg.data[7]
+    ### As we don't use index [3], [4], [5]                                   
+    ### Make new data array                                                   
+    ### msg.data[3], msg.data[4], msg.data[5] is always 0 in this situation   
+  
+    xycar_sensor.data[0] = msg.data[0]
+    xycar_sensor.data[1] = msg.data[1]
+    xycar_sensor.data[2] = msg.data[2]
+    xycar_sensor.data[3] = msg.data[6]
+    xycar_sensor.data[4] = msg.data[7]
 
     print(msg.data)
   
 
-def collision():
-    global accident
-    for i in xycar_sub.data:
 
-         ## radius of car's boundary circle is 20*sqrt(13) (=about 48)
-         ## 2 = additional space
 
-        if i < 48 + 2 and 0 < i: 
-            accident = True
+###                                          ### 
+### Get valueForCentering to make angle      ###
+### We want to make 'valueForCentering == 0' ### 
+### Do PID control using valueForCentering   ###   
+###                                          ###         
 
-    return accident        
+def get_valueForCentering():
+    global valueForCentering
 
+    right_sensor = xycar_sensor.data[3] + xycar_sensor.data[2]
+    left_sensor = xycar_sensor.data[4] + xycar_sensor.data[0]
+   
+    valueForCentering  = float(right_sensor) - float(left_sensor)
+
+    return valueForCentering
+
+
+
+
+###                                                        ### 
+### Get 'error' and 'errorControl' value for PID control   ###
+###                                                        ### 
 
 def get_errorControl(vector):
-    ## record error
-    global errorList
-    global error
+    global error, errorList          ## record error
     
     desired_vector = 0
-    # errorSum = 0
+    
     error = vector - desired_vector
+
+    if error > 500:                 ## compressed error (help D control)
+        error = error * 0.5
+
     errorList.insert(0, error)
     errorPrev = errorList[1]
 
     errorControl = error - errorPrev
-    errorList.pop()
+
+    errorList.pop()                 ## errorList[1] is always errorPrev, errorList[[0] is always error(current error)
     
     return errorControl
 
 
-def get_dt():
-    global timeList
-
-    error_Time = rospy.Time.from_sec(time.time())
-    timeList.insert(0, error_Time.to_sec())
-    errorPrev_Time = timeList[1]
-    dt = error_Time.to_sec() - errorPrev_Time
-    timeList.pop()
-
-    return dt
 
 
+###                                                                                  ###
+### Following codes are for PID control                                              ###
+###                                                                                  ###
+### If you want to add 'I' control, add following codes in function                  ###
+### If you don't add, it's PD control                                                ###
+###                                                                                  ###
+### ki = float(11) / 200    ## golden ratio ki for I control                         ###
+### integral_output = 0                                                              ###
+### integral_output = integral_output + ki * error                                   ###
+### presaturated_output = proportional_output + derivative_output + integral_output  ###
+###                                                                                  ###
 
-def calculate_PID(error, errorControl, dt):
+def calculate_PD(error, errorControl): 
+    global constant_PresaturatedToAngle
 
-    kp = 0.81 #float(17) / 200
-    #ki = float(11) / 200
-    kd = float(17) / 200
+    kp = 10.0                                               ## golden ratio kp for P control
+    kd = 5.0                                                ## golden ratio kd for D control
+    constant_PresaturatedToAngle =  float(1) / 46           ## constant_PresaturatedToAngle size is related with kp
+    
+    proportional_output = kp * error                        ## P control
+    derivative_output = kd * (errorControl)                 ## D control
+    presaturated_output = proportional_output + derivative_output       ## PD control
 
-    integral_output = 0
 
-    proportional_output = kp * error #* 9 / 100
-    #integral_output = integral_output + ki * error * dt
-    derivative_output = kd * (errorControl / dt)
-
-    presaturated_output = proportional_output + derivative_output 
-    # presaturated_output = proportional_output + derivative_output + integral_output
-
-    print("presaturated_output")
-    print(presaturated_output)
-
+    
     return presaturated_output
 
 
-# rth = ROSTopicHz.get_hz(topic=/ultrasonic) 
+
+
+###                                     ### 
+### Chcek the possibility of accident   ###
+###                                     ### 
+
+def check_collision():
+    global accidentPossibility, collisionRange
+
+    carWidth = 48
+    carLength = 136
+    carDiagonal = math.sqrt(carWidth * carWidth + carLength * carLength)    ## radius of car's boundary circle is about 20*sqrt(13) in this situation
+    maxSafetyRange = (carDiagonal - carWidth) / 2                           ## maxSafetyRange is about 48 in this situation
+    additionalSpace = 2
+
+    collisionRange = maxSafetyRange + additionalSpace         
+                                    
+    for sensorLength in xycar_sensor.data:
+        if sensorLength < collisionRange and 0 < sensorLength:           
+            accidentPossibility = True
+
+    return accidentPossibility        
+
+
+
+
+###                                       ###
+### Avoiding obstacle (escape collision)  ###
+###                                       ###
+
+def escape_collision():
+    global turn, presaturated_output, accidentPossibility, collisionRange, constant_PresaturatedToAngle
+
+    accidentPossibility_sensor = min(xycar_sensor.data)
+    accidentPossibility_sensor_idx = xycar_sensor.data.index(accidentPossibility_sensor)
+
+    while collisionRange > xycar_sensor.data[accidentPossibility_sensor_idx]:         
+            velocity = -100
+            if turn == 'RIGHT':
+                angle = -presaturated_output * constant_PresaturatedToAngle
+            elif turn == 'LEFT':
+                angle = +presaturated_output * constant_PresaturatedToAngle
+            else:
+                turn = 'STRAIGHT'
+                angle = 0  
+
+            xycar_msg.data = [angle, velocity]
+            motor_pub.publish(xycar_msg)  
+
+    accidentPossibility = False 
+
+
+##
+## initialize
+##
+xycar_sensor = Int32MultiArray()
+xycar_sensor.data = [0,0,0,0,0]
+errorList = [0]
+accidentPossibility = False
+angle = 0
+velocity = 100      ## you can change velocity  
+error = 0.0
+
 rospy.init_node('guide')
 motor_pub = rospy.Publisher('xycar_motor_msg', Int32MultiArray, queue_size=1)
 ultra_sub = rospy.Subscriber('ultrasonic', Int32MultiArray, callback)
@@ -100,60 +166,25 @@ xycar_msg = Int32MultiArray()
 
 while not rospy.is_shutdown():
 
-    val = 1.0
-    angle_cur = 0
-    velocity = 100  
-    error = 0.0
+    valueForCentering = get_valueForCentering()
+    errorControl = get_errorControl(valueForCentering)
+    presaturated_output = abs(calculate_PD(error, errorControl))
 
-    right_sensor = xycar_sub.data[3] + xycar_sub.data[2]
-    left_sensor = xycar_sub.data[4] + xycar_sub.data[0]
-
-    if left_sensor == 0 or right_sensor == 0:
-        left_sensor = 1
-        right_sensor = 1
-   
-    vector  = float(right_sensor) - float(left_sensor)
-
-
-    errorControl = get_errorControl(vector)
-    dt = get_dt()
-    presaturated_output = calculate_PID(error, errorControl, dt)
-
-
-    if vector > 0:
-        angle_cur = presaturated_output
+    if valueForCentering > 0:
         turn = 'RIGHT'
+        angle = presaturated_output * constant_PresaturatedToAngle
 
-    elif vector < 0:
-        angle_cur = presaturated_output
+    elif valueForCentering < 0:
         turn = 'LEFT'
+        angle = -presaturated_output * constant_PresaturatedToAngle
 
     else:
         turn = 'STRAIGHT'
-        angle_cur = 0    
+        angle = 0    
 
 
-    if collision() == True:
-        temp = min(xycar_sub.data)
-        idx = xycar_sub.data.index(temp)
+    if check_collision() == True:
+        escape_collision()  
 
-        while float(temp)+0.32 > xycar_sub.data[idx]: # 1 rate would be best!
-            velocity = -100
-            if turn == 'RIGHT':
-                angle_cur = -presaturated_output 
-            elif turn == 'LEFT':
-                angle_cur = -presaturated_output
-            else:
-                turn = 'STRAIGHT'
-                angle_cur = 0  
-            print("collision presaturated_output")
-            print(presaturated_output)  
-            xycar_msg.data = [angle_cur, velocity]
-            motor_pub.publish(xycar_msg)  
-        accident = False    
-        print("collision angle_cur")
-        print(angle_cur)
-    print("angle_cur")
-    print(angle_cur)
-    xycar_msg.data = [angle_cur, velocity]
+    xycar_msg.data = [angle, velocity]
     motor_pub.publish(xycar_msg)
